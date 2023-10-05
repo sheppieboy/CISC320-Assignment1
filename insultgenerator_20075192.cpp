9692160d51dda75fe45d2b70b6900ea3b77016c7
Luke Sheppard <sheppiegit@gmail.com>
2023-10-04 22:00:14 -0400

    Added implementation of NumInsultsOutOfBounds class.

--- insultgenerator_20075192.cpp
+++ insultgenerator_20075192.cpp
@@ -3,13 +3,24 @@
 
 using namespace std;
 
+//File Exception class implementation
 FileException::FileException(const string& file_error) : file_error(file_error){};
 
+//what function for outputting file_error string
 string& FileException::what(){
     return file_error;
 }
 
+//Num Insults Out of Bounds class implementation
 NumInsultsOutOfBounds::NumInsultsOutOfBounds(const string& num_error) : num_error(num_error){};
+
+//what function for outputting num_error string
+string& NumInsultsOutOfBounds::what(){
+    return num_error;
+}
+
+
+
 
 
 
